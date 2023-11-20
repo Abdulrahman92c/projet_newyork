d7bb04733ff7825ae38bb925fd746de3bc389947
Abdulrahman92c <99719388+Abdulrahman92c@users.noreply.github.com>
2023-11-20 14:38:39 +0100

    up JS

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-import { Desciption } from "./template.js";
+import { Description } from "./template.js";
 
 document.addEventListener("DOMContentLoaded", e => {
     console.log(e.target);
-    console.table(Desciption);
+    console.table(Description);
 });
--- src/app.js
+++ src/app.js
@@ -4,5 +4,6 @@ import { Description } from "./template.js";
 document.addEventListener("DOMContentLoaded", e => {
     console.log(e.target);
     console.table(Description);
+
     
 });
