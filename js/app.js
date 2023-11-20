6fcb1925207290ee05be94c561be45a6b0d253dc
Abdulrahman92c <99719388+Abdulrahman92c@users.noreply.github.com>
2023-11-20 14:14:12 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+import { Desciption } from "./template.js";
+
+document.addEventListener("DOMContentLoaded", e => {
+    console.log(e.target);
+    console.table(Desciption);
+});
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import { Desciption } from "./template.js";
+
+
+document.addEventListener("DOMContentLoaded", e => {
+    console.log(e.target);
+    console.table(Desciption);
+    
+});
