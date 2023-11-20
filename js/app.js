4973d0c13ddf3dcaf6c2b7d17fa86f508fa6bd23
Abdulrahman92c <99719388+Abdulrahman92c@users.noreply.github.com>
2023-11-20 17:32:23 +0100

    update js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 
 import { Description } from "./template.js";
+import { template } from "./template.js";
 
 document.addEventListener("DOMContentLoaded", e => {
     console.log(e.target);
     console.table(Description);
+    template.printF();
 });
--- js/template.js
+++ js/template.js
@@ -1,5 +1,5 @@
 export const Description = {
-    image: "./asset/n-y.jpg",
+    Image: "./asset/n-y.jpg",
     description: `New York est une ville composée de 5 arrondissements à l'embouchure du fleuve Hudson et de l'océan Atlantique. En son centre se trouve Manhattan, un arrondissement densément peuplé faisant partie des principaux centres commerciaux, financiers et culturels du monde. Ses sites incontournables comprennent des gratte-ciel comme l'Empire State Building et l'immense Central Park. Le théâtre de Broadway est situé sur Times Square`,
     Météo: `6°C, vent O à 6 km/h, 68 % d'humidité weather.com`,
     Quartier: `Harlem, Upper East Side, West Village, Flushing, SoHo, PLUS`,
@@ -8,9 +8,18 @@ export const Description = {
     Coordonnées: `40° 42′ 51″ nord, 74° 00′ 21″ ouest `
 };
 
-const json = JSON.stringify(Description);
-console.log(json + "\n");
+// const json = JSON.stringify(Description)
+// console.log(json + "\n")
 
-for (let index in Description) {
-    console.log(index + " " + Description[index] + '\n');
-}
+export const template = {
+    el: document.querySelector("section"),
+    printF() {
+        for (let index in Description) {
+            let value = Description[index];
+            console.log[index];
+            index == "Image" ? this.el.innerHTML += `<img src="${value}" alt="image"></img>` : this.el.innerHTML += `<p> 
+                            <strong>${index}</strong> :${value}  
+                            </p>`;
+        }
+    }
+};
