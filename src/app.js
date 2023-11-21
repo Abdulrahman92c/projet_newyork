92c8b68d69e936c1aa3f1dad8bdf795c5a96a063
Abdulrahman92c <99719388+Abdulrahman92c@users.noreply.github.com>
2023-11-21 14:44:14 +0100

    update du folder

--- src/app.js
+++
@@ -1,9 +0,0 @@
-
-import { Description } from "./template.js";
-import {template} from "./template.js";
-
-document.addEventListener("DOMContentLoaded", e => {
-    console.log(e.target);
-    console.table(Description);
-    template.printF();
-});
